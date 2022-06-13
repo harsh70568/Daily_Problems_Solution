dae6059d58bb451984116009f52e29ada3b998c6
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-13 21:21:30 +0530

    Time: 46 ms (16.46%), Space: 16.8 MB (86.62%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int do_xor = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            do_xor ^= nums[i];
+        }
+        return do_xor;
+    }
+};
