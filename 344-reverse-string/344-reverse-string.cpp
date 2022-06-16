866852ba5aba48356063e96785788e7c1c7fa882
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-06-16 11:20:46 +0530

    Time: 49 ms (11.07%), Space: 23.1 MB (93.81%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    void solve(vector<char> &s, int  i, int j)
+    {
+        if(i >= j)
+        {
+            return;
+        }
+        swap(s[i++],s[j--]);
+        solve(s,i,j);
+    }
     void reverseString(vector<char>& s) {
         int i = 0;
         int j = s.size()-1;
-        while(i < j)
-        {
-            swap(s[i],s[j]);
-            i++;
-            j--;
-        }
+        solve(s,i,j);
     }
 };
