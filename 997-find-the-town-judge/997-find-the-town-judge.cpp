2eff9ee2ce20c4b3274e8a1d96062bdb70f77ea6
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-07-03 22:46:04 +0530

    Time: 234 ms (65.30%), Space: 69 MB (21.81%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> Indegree(n+1,0);
+        vector<int> outdegree(n+1,0);
+        for(auto x : trust)
+        {
+            outdegree[x[0]]++;
+            Indegree[x[1]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(outdegree[i]==0 && Indegree[i]==n-1) return i; // if any people satify the given condition then current people is judge
+        }
+        return -1; 
+    }
+};
