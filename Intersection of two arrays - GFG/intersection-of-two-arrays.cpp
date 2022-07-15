13a0e22127ce1233040c57f787a714bc15cae7af
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-07-15 21:16:07 +0530

    Added solution - LeetHub

--- Intersection of two arrays - GFG/intersection-of-two-arrays.cpp
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.cpp
@@ -14,11 +14,12 @@ class Solution {
     int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
         // Your code goes here
         set<int> s;
+        int count = 0;
         for(int i = 0; i < n; i++)
         {
             s.insert(a[i]);
         }
-        int count = 0;
+        
         for(int i = 0; i < m; i++)
         {
             if(s.find(b[i]) != s.end())
