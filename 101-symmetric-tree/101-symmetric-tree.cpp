2b683bcec23aa226b016c8b8259f9386976fc61f
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-07-18 19:00:38 +0530

    Time: 11 ms (38.49%), Space: 16.4 MB (78.19%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -15,10 +15,11 @@ public:
     {
         if(root1 == NULL || root2 == NULL) return (root1 == root2);
         if(root1->val != root2->val) return false;
+        
         return solve(root1->left,root2->right) && solve(root1->right,root2->left);
+            
     }
     bool isSymmetric(TreeNode* root) {
         return solve(root->left,root->right);
-        
     }
 };
