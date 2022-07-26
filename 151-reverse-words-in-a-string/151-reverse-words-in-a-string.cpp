027c2ff3c030bf2cee20ad848e497c91f711cba5
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-07-26 22:53:59 +0530

    Time: 4 ms (84.21%), Space: 7.3 MB (75.52%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    bool check(string str)
-    {
-        for(int i = 0; i < str.length(); i++)
-        {
-            if(str[i] != ' ')
-            {
-                return true;
-            }
-        }
-        return false;
-    }
     string reverseWords(string s) {
+        string temp = "";
         string ans = "";
-        string str = "";
-        int n = s.length(); 
-        int i = n-1;
-        while(i >= 0)
+        int n = s.length();
+        for(int i = n-1; i >= 0; i--)
         {
-            if(s[i] == ' ' && check(str))
+            if(s[i] == ' ' && temp != "")
             {
-                reverse(str.begin(),str.end());
-                ans.append(str);
-                ans.append(" ");
-                str = "";
+                reverse(temp.begin(),temp.end());
+                ans += temp;
+                ans += " ";
+                temp = "";
             }
             else if(s[i] != ' ')
             {
-                str += s[i];
+                temp.push_back(s[i]);
             }
-            i--;
+        }
+        if(temp != "")
+        {
+            reverse(temp.begin(),temp.end());
+            ans += temp;
         }
         if(s[0] == ' ') ans.pop_back();
-            else
-            {
-                reverse(str.begin(),str.end());
-                ans.append(str);
-            }
         return ans;
     }
 };
