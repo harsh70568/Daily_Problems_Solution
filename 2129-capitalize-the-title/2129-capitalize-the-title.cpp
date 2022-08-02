eb3f73be5674124e03128a8d90e20c9d735976f0
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-02 22:21:02 +0530

    Time: 3 ms (63.12%), Space: 6.4 MB (28.79%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -21,16 +21,11 @@ public:
                 temp += tolower(title[i]);
             }
         }
-        transform(temp.begin(),temp.end(),temp.begin(),::tolower);
-        if(temp.length() == 1 || temp.length() == 2)
-        {
-            ans += temp;
-        }
-        else
+        if(temp.length() > 2)
         {
             temp[0] = temp[0]-32;
-            ans += temp;
         }
+        ans += temp;
         return ans;
     }
 };
