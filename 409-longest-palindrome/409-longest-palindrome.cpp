2551f42ee178bf9fd3e57d8719f1639848b7a9c5
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-02 21:54:11 +0530

    Time: 10 ms (15.23%), Space: 6.6 MB (50.50%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<int,int> mpp;
+        unordered_map<char,int> mpp;
+        int sum = 0;
         for(int i = 0; i < s.length(); i++)
         {
             mpp[s[i]]++;
         }
-        int sum = 0;
-        bool odd = false;
+        bool is_first_odd = false;
         for(auto it : mpp)
         {
             if(it.second % 2 == 0)
@@ -16,11 +16,17 @@ public:
             }
             else
             {
-                sum += it.second-1;
-                odd = true;
+                if(is_first_odd == false)
+                {
+                    sum += it.second;
+                    is_first_odd = true;
+                }
+                else
+                {
+                    sum += it.second-1;
+                }
             }
         }
-        if(odd) return sum+1;
-        else return sum;
+        return sum;
     }
 };
