a52203b4935d8445ccbb98002208cfb2be4cf484
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-05 11:42:14 +0530

    Time: 7 ms (17.13%), Space: 13.6 MB (6.20%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void solve(vector<int> candidates, vector<vector<int>> &ans, vector<int> temp, int k, int n,int index)
+    void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> temp, int index, int k, int n)
     {
-        if(index >= candidates.size())
+        if(index >= nums.size())
         {
             if(temp.size() == k && n == 0)
             {
@@ -11,25 +11,26 @@ public:
             return;
         }
         
-        if(candidates[index] <= n)
+        if(nums[index] <= n)
         {
-            temp.push_back(candidates[index]);
-            solve(candidates,ans,temp,k,n-candidates[index],index+1);
+            temp.push_back(nums[index]);
+            solve(nums,ans,temp,index+1,k,n-nums[index]);
             temp.pop_back();
         }
         
-        solve(candidates,ans,temp,k,n,index+1);
+        solve(nums,ans,temp,index+1,k,n);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> candidates;
+        vector<int> nums;
         for(int i = 1; i <= 9; i++)
         {
-            candidates.push_back(i);
+            nums.push_back(i);
         }
+        
         vector<vector<int>> ans;
         vector<int> temp;
         int index = 0;
-        solve(candidates,ans,temp,k,n,index);
+        solve(nums,ans,temp,index,k,n);
         return ans;
     }
 };
