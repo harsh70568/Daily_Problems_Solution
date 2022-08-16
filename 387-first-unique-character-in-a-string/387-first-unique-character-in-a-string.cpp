96ed4b98ed67586cafa51504102e4d144050c50d
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-16 21:54:23 +0530

    Time: 102 ms (12.72%), Space: 10.7 MB (44.39%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> mpp;
+        for(int i = 0; i < s.length(); i++)
+        {
+            mpp[s[i]]++;
+        }
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(mpp[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
