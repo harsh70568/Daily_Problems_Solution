e4a90b6eb5e7edf02dd27e4c29ac9524ab94370c
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-21 18:05:17 +0530

    Time: 7 ms (39.85%), Space: 7.8 MB (44.50%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, set<pair<int,int>> &s, vector<vector<int>> &dp)
+    int solve(int i, int j, int m, int n, vector<vector<int>> &dp, vector<vector<int>> &obstacleGrid)
     {
-        if(s.find({i,j}) != s.end()) return 0;
+        if(i < m && j < n && obstacleGrid[i][j] == 1) return 0;
         if(i == m-1 && j == n-1) return 1;
         if(i > m-1 || j > n-1) return 0; 
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        int right = solve(i,j+1,m,n,s,dp);
-        int down = solve(i+1,j,m,n,s,dp);
+        int right = solve(i,j+1,m,n,dp,obstacleGrid);
+        int down = solve(i+1,j,m,n,dp,obstacleGrid);
         return dp[i][j] = right+down;
         
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        set<pair<int,int>> s;
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int>(n,-1));
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(obstacleGrid[i][j] == 1)
-                {
-                    s.insert({i,j});
-                }
-            }
-        }
-        return solve(0,0,m,n,s,dp);
+        return solve(0,0,m,n,dp,obstacleGrid);
     }
 };
