3874c0a0bf267d5dd27a046620e05e5b78aaaf91
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-22 16:56:56 +0530

    Time: 8 ms (70.15%), Space: 9.1 MB (31.66%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int recursion(vector<vector<int>> &triangle,int n,int m,vector<vector<int>>&dp) {
-        if(n==triangle.size()-1) return dp[n][m]=triangle[n][m];
-        if(dp[n][m]!=-1) return dp[n][m];
-        int ans1=triangle[n][m]+recursion(triangle,n+1,m,dp);
-        int ans2=triangle[n][m]+recursion(triangle,n+1,m+1,dp);
-        return dp[n][m]=min(ans1,ans2);
+    int solve(int i, int j, int n, vector<vector<int>> &triangle, vector<vector<int>> &dp)
+    {
+        if(i == n-1) return triangle[i][j];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int down = triangle[i][j] + solve(i+1,j,n,triangle,dp);
+        int right = triangle[i][j] + solve(i+1,j+1,n,triangle,dp);
+        
+        return dp[i][j] = min(down,right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        int m=triangle[n-1].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return recursion(triangle,0,0,dp);
-        
-        
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return solve(0,0,n,triangle,dp);
     }
 };
