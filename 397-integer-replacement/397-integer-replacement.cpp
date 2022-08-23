4ffe9fe4da7b96eb59ae3108ad194e01b274adf4
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-23 13:51:25 +0530

    Time: 7 ms (25.42%), Space: 6 MB (53.08%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -3,17 +3,17 @@ public:
     int integerReplacement(int n) {
         if(n == INT_MAX) return 32;
         if(n == 1) return 0;
-
+        int mini = INT_MAX;
         if(n % 2 == 0)
         {
-            return 1 + integerReplacement(n/2);
+            mini =  1 + integerReplacement(n/2);
         }
         else
         {
-            int odd1 = 1 + integerReplacement(n-1);
-            int odd2 = 1 + integerReplacement(n+1);
-            return min(odd1,odd2);
+            mini = min(mini, 1 + integerReplacement(n-1));
+            mini = min(mini,1 + integerReplacement(n+1));
+            
         }
-        
+        return mini;
     }
 };
