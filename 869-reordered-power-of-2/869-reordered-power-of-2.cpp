0775ac5809c8c6184de27e2e7d7e695665f2eb87
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-26 19:17:28 +0530

    Time: 7 ms (42.94%), Space: 6.6 MB (24.29%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    void solve(string num, int index, bool &flag)
-    {
-        if(index >= num.length())
-        {
-            if(num[0] != '0'){
-            int num1 = stoi(num);
-            if((num1 & num1-1) == 0) flag = true;
-            }
-            return;
-        }
-        
-        for(int i = index; i < num.length(); i++)
-        {
-            swap(num[index],num[i]);
-            solve(num,index+1,flag);
-            swap(num[index],num[i]);
-        }
-    }
     bool reorderedPowerOf2(int n) {
         string num = to_string(n);
         
-        int index = 0;
-        bool flag = false;
-        solve(num,index,flag);
-        return flag;
+        sort(num.begin(),num.end());
+        
+        vector<string> power;
+        for(int i = 0; i <= 30; i++)
+        {
+            int p = pow(2,i);
+            power.push_back(to_string(p));
+        }
+        
+        for(int i = 0; i <= 30; i++)
+        {
+            sort(power[i].begin(),power[i].end());
+        }
+        
+        
+        for(int i = 0; i <= 30; i++)
+        {
+            if(power[i] == num) return true;
+        }
+        return false;
+        
     }
 };
