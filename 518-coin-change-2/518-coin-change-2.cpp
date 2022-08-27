ba703780df98f0d46f9440a85fdc92a0f38b8ecb
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-08-27 20:34:08 +0530

    Time: 11 ms (92.96%), Space: 18.5 MB (44.67%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(int index, int amount, vector<int> &coins, vector<vector<int>> &dp)
+    {
+        if(index == 0)
+        {
+            if(amount % coins[0] == 0) return 1;
+            else return 0;
+        }
+        if(dp[index][amount] != -1) return dp[index][amount];
+        int not_take = solve(index-1,amount,coins,dp);
+        
+        int take = 0;
+        if(coins[index] <= amount)
+        {
+            take = solve(index,amount-coins[index],coins,dp);
+        }
+        
+        return dp[index][amount] = take+not_take;
+    }
+    
+    
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        return solve(n-1,amount,coins,dp);
+    }
+};
