d5929d2b908a25d1a4a1bf2b17ef0cc743ca2632
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-04 23:40:32 +0530

    Time: 176 ms (7.40%), Space: 27.2 MB (29.09%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -5,10 +5,9 @@ public:
         if(i < 0 || j < 0) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
-        
         if(s[i] == t[j])
         {
-            return 1 + solve(i-1,j-1,s,t,dp);
+            return dp[i][j] = 1 + solve(i-1,j-1,s,t,dp);
         }
         
         return dp[i][j] = max(solve(i-1,j,s,t,dp),solve(i,j-1,s,t,dp));
@@ -16,11 +15,10 @@ public:
     int minInsertions(string s) {
         string t = s;
         reverse(t.begin(),t.end());
-        
         int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        int ans = solve(n-1,n-1,s,t,dp);
         
-        vector<vector<int>> dp(n, vector<int> (n+1,-1));
-        
-        return n - solve(n-1,n-1,s,t,dp);
+        return n - ans;
     }
 };
