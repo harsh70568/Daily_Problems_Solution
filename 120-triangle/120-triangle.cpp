6da9c092fb2f06091f8b11ba768fd776ce10c00c
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-05 23:47:46 +0530

    Time: 19 ms (7.95%), Space: 9.1 MB (10.22%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int j, int n, vector<vector<int>> &triangle, vector<vector<int>> &dp)
+    int solve(int i, int j,int n, vector<vector<int>> &triangle, vector<vector<int>> &dp)
     {
         if(i == n-1) return triangle[i][j];
         
         if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = triangle[i][j] + solve(i+1,j,n,triangle,dp);
+        int left = triangle[i][j] + solve(i+1,j,n,triangle,dp);
         int right = triangle[i][j] + solve(i+1,j+1,n,triangle,dp);
         
-        return dp[i][j] = min(down,right);
+        return dp[i][j] = min(left,right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
+        
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
         return solve(0,0,n,triangle,dp);
     }
 };
