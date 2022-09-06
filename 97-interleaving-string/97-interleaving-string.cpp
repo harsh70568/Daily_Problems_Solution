0028a27271a12790ed8d6bd28d1ae97afb791048
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-07 01:27:28 +0530

    Time: 27 ms (35.81%), Space: 25.9 MB (24.10%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool solve(int i, int j, int k, string a, string b, string c,int n1, int n2, int n3, vector<vector<int>> &dp)
+    bool solve(int i, int j, int k, string a, string b, string c, vector<vector<int>> &dp)
     {
-        if(k == n3) return true;
+        if(k == c.length()) return true;
         if(dp[i][j] != -1) return dp[i][j];
         
         if(a[i] == c[k] && b[j] == c[k])
         {
-            return dp[i][j] = solve(i+1,j,k+1,a,b,c,n1,n2,n3,dp) || solve(i,j+1,k+1,a,b,c,n1,n2,n3,dp);
+            return dp[i][j] = solve(i+1,j,k+1,a,b,c,dp) || solve(i,j+1,k+1,a,b,c,dp);
         }
         else if(i < a.length() && a[i] == c[k])
         {
-            return dp[i][j] = solve(i+1,j,k+1,a,b,c,n1,n2,n3,dp);
+            return dp[i][j] = solve(i+1,j,k+1,a,b,c,dp);
         }
-        else if(j < b.length() && b[j] == c[k])
+        else if(b[j] == c[k])
         {
-            return dp[i][j] = solve(i,j+1,k+1,a,b,c,n1,n2,n3,dp);
+            return dp[i][j] = solve(i,j+1,k+1,a,b,c,dp);
         }
         else return dp[i][j] = false;
     }
@@ -26,6 +26,6 @@ public:
         int k = s3.length();
         vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
         if(m+n != k) return false;
-        return solve(0,0,0,s1,s2,s3,m,n,k,dp);
+        return solve(0,0,0,s1,s2,s3,dp);
     }
 };
