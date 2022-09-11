c4d80847db0731ccf26f9163814a4b67d48a0122
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-11 16:13:26 +0530

    Time: 24 ms (31.75%), Space: 18.3 MB (14.33%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    bool solve(int i, int j, string s, string p)
+    bool solve(int i, int j, string s, string p, vector<vector<int>> &dp)
     {
         if(i < 0 && j < 0) return true;
         if(i >= 0 && j < 0) return false;
         if(i < 0 && j >= 0)
         {
-            if(p[j] == '*') return solve(i,j-2,s,p);
+            if(p[j] == '*') return solve(i,j-2,s,p,dp);
             else return false;
         }
+        
+        if(dp[i][j] != -1) return dp[i][j];
         if(s[i] == p[j] || p[j] == '.')
         {
-            return solve(i-1,j-1,s,p);
+            return dp[i][j] = solve(i-1,j-1,s,p,dp);
         }
         
         if(p[j] == '*')
         {
             if(j > 0 && (p[j-1] == '.' || p[j-1] == s[i]))
             {
-                return solve(i-1,j,s,p) || solve(i,j-2,s,p);
+                return dp[i][j] = solve(i-1,j,s,p,dp) || solve(i,j-2,s,p,dp);
             }
-            else return solve(i,j-2,s,p);
+            else return dp[i][j] = solve(i,j-2,s,p,dp);
         }
         
-        return false;
+        return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        
-        return solve(n-1,m-1,s,p);
+        vector<vector<int>> dp(n, vector<int>(m+1,-1));
+        return solve(n-1,m-1,s,p,dp);
     }
 };
