8f548d757ca38e1248e06f61389c3f85c8f5ac52
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-22 21:23:39 +0530

    Time: 54 ms (13.98%), Space: 11.4 MB (28.33%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
-        string temp = "";
         string ans = "";
-        for(int i = 0; i < n; i++)
+        string temp = "";
+        for(int i = 0; i < s.length(); i++)
         {
             if(s[i] == ' ')
             {
                 reverse(temp.begin(),temp.end());
                 ans += temp;
-                ans += ' ';
+                ans += " ";
                 temp = "";
             }
             else
