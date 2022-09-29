6ea6e683f914494afdd7a0d3a2af1fe9e6dbf576
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-09-29 11:48:16 +0530

    Time: 204 ms (13.25%), Space: 37.3 MB (12.87%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-   static bool myCompare(pair<int, int> &a, pair<int, int> &b)
+    static bool cmp(pair<int,int> &a, pair<int,int> &b)
     {
-        if(a.second== b.second)
-        {
-            return a.first < b.first;
-        }
+        if(a.second == b.second) return a.first < b.first;
         return a.second < b.second;
     }
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) 
-    {
-        vector<int> ans;
-        vector<pair<int, int>> temp;
-        for(int i=0; i< arr.size(); i++)
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> res;
+        vector<pair<int,int>> ans;
+        
+        for(int i = 0; i < arr.size(); i++)
         {
-            temp.push_back({arr[i], abs(arr[i]- x)});
+            ans.push_back({arr[i], abs(arr[i] - x)});
         }
-        sort(temp.begin(), temp.end(), myCompare);
-        int i=0;
-        while(k--)
+        
+        sort(ans.begin(),ans.end(),cmp);
+        int n = ans.size();
+        int size = min(k,n);
+        for(int i = 0; i < size; i++)
         {
-            ans.push_back(temp[i++].first);
+            res.push_back(ans[i].first);
         }
-        sort(ans.begin(), ans.end());
-        return ans;
+        
+        sort(res.begin(),res.end());
+        
+        return res;
     }
 };
