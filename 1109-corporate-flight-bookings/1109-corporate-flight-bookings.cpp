b7f81226b2c41aaad85aea5cde8f0c52bdb6c4fa
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-10-01 13:01:41 +0530

    Time: 377 ms (53.16%), Space: 67.5 MB (97.50%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> ans(n+1,0);
+        
+        for(int i = 0; i < bookings.size(); i++)
+        {
+            ans[bookings[i][0]-1] += bookings[i][2];
+            ans[bookings[i][1]] -= bookings[i][2];
+        }
+        
+        for(int i = 1; i < n+1; i++)
+        {
+            ans[i] = ans[i-1] + ans[i];
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
