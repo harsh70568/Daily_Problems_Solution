391cbbee36a92fe53440daa98bf51a00ad693fa0
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-10-07 14:36:48 +0530

    Time: 1263 ms (5.14%), Space: 264.9 MB (8.47%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    
-    int lcs(int i, int currSize, int currSum, vector<vector<int>> &dp, string &s, int k)
+    int solve(int index, int sum, int count, string &s, int k, vector<vector<int>> &dp)
     {
-            if (i<0)    return 0;
-            
-            if (dp[i][currSize] != -1)  return dp[i][currSize];    
-
-            //  not take
-            int c1 = lcs(i - 1, currSize, currSum, dp, s, k) + 0;
-            int c2 = 0;
-            if (currSum + (s[i]-'0')* pow(2,currSize) <= k)
-                c2 = lcs(i - 1, currSize + 1, currSum + (s[i]-'0')* pow(2,currSize), dp, s, k) + 1;
-                    
-            return dp[i][currSize] = max(c1, c2);
+        if(index < 0) return 0;
+        if(dp[index][count] != -1) return dp[index][count];
         
-    }        
+        int not_take = 0 + solve(index-1, sum, count, s, k, dp);
+        int take = 0;
+        if(sum + (s[index] - '0') * pow(2,count) <= k)
+        {
+            take = 1 + solve(index-1, sum + (s[index] - '0')*pow(2,count), count+1, s, k, dp);
+        }
         
-        
-        
+        return dp[index][count] = max(take,not_take);
+    }
     int longestSubsequence(string s, int k) {
-        int n = s.size();
-        vector<vector<int>>dp(n,vector<int>(n, -1));
-        return lcs(n - 1, 0, 0, dp, s, k);
-        
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        return solve(n-1, 0, 0, s, k, dp);
     }
 };
