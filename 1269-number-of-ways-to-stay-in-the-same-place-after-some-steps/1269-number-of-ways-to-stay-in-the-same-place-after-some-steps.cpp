878d0991af335ef3018ce92ce7c2a10245c2c9ed
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-10-08 14:39:58 +0530

    Time: 62 ms (29.82%), Space: 14.6 MB (25.96%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    long long mod = 1e9+7;
+    int dp[2005][1005];
+    int solve(int index, int steps, int arrLen)
+    {
+        if(index < 0 || index > arrLen-1) return 0;
+        if(steps == 0)
+        {
+            if(index == 0) return 1;
+            else return 0;
+        }
+        
+        if(dp[index][steps] != -1) return dp[index][steps];
+        
+        long long right = 0;
+        long long left = 0;
+        if(index < arrLen-1)
+        {
+            right = solve(index+1, steps-1, arrLen) % mod;
+        }
+        if(index > 0)
+        {
+            left = solve(index-1, steps-1, arrLen) % mod;
+        }
+        long long stay = solve(index, steps-1, arrLen) % mod;
+        
+        return dp[index][steps] = (right + left + stay) % mod;
+    }
+    int numWays(int steps, int arrLen) {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, steps, arrLen);
+    }
+};
