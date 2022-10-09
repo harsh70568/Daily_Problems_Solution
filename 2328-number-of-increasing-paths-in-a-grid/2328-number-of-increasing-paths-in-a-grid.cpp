8b62d67a161894f2320cd901d75c87f67e2bb8d7
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-10-09 18:59:31 +0530

    Time: 525 ms (45.38%), Space: 57.6 MB (23.64%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int solve(int i, int j, vector<vector<int>> &grid, int m, int n, vector<vector<int>> &dp)
+    {
+        if(i < 0 || j < 0 || i > m-1 || j > n-1) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        
+        int left = 0;
+        int right = 0;
+        int down = 0;
+        int up = 0;
+        
+        if(j >= 1 && grid[i][j-1] > grid[i][j])
+        {
+            left = 1 + solve(i, j-1, grid, m, n, dp) % mod;
+        }
+        if(j < n-1 && grid[i][j+1] > grid[i][j])
+        {
+            right = 1 + solve(i, j+1, grid, m, n, dp) % mod;
+        }
+        if(i < m-1 && grid[i+1][j] > grid[i][j])
+        {
+            down = 1 + solve(i+1, j, grid, m, n, dp) % mod;
+        }
+        if(i >= 1 && grid[i-1][j] > grid[i][j])
+        {
+            up = 1 + solve(i-1, j, grid, m, n, dp) % mod;
+        }
+        
+        return dp[i][j] = (right + left + down + up) % mod;
+    }
+    int countPaths(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = 0;
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                total += solve(i, j, grid, m, n, dp);
+                total %= mod;
+            }
+        }
+        //total = total % mod;
+        
+        return total + (m*n);
+    }
+};
