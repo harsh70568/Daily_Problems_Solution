6f2c7ca5aeb95d585b4fd02617db468ba289b617
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-10-19 23:31:40 +0530

    Time: 83 ms (5.39%), Space: 22.3 MB (5.21%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool check(stack<char> st, string s)
+    {
+        int j = s.length()-1;
+        while(j >= 0 && s[j] == st.top() && !st.empty())
+        {
+            st.pop();
+            j--;
+        }
+        
+        return (j == -1);
+        
+    }
+    string removeOccurrences(string s, string part) {
+        int n = part.length();
+        stack<char> st;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            st.push(s[i]);
+            if(st.size() >= n){
+            if(check(st, part))
+            {
+                int size = n;
+                while(size--)
+                {
+                    st.pop();
+                }
+            }
+            }
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
