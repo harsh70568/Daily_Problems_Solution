5761c9d66abc5353ce7a3ffcbafc09e8a2a8c749
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-11-04 01:48:49 +0530

    Time: 710 ms (16.83%), Space: 151.7 MB (8.02%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(int index, int n, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if(index == 0)
+        {
+            if(n % nums[index] == 0) return n / nums[index];
+            else return 1e9;
+        }
+        
+        if(dp[index][n] != -1) return dp[index][n];
+        
+        int not_pick = 0 + solve(index-1, n, nums, dp);
+        int pick = 1e9;
+        if(nums[index] <= n)
+        {
+            pick = 1 + solve(index, n - nums[index], nums, dp);
+        }
+        
+        return dp[index][n] = min(pick, not_pick);
+    }
+    int numSquares(int n) {
+        vector<int> ans;
+        for(int i = 1; i <= sqrt(n); i++)
+        {
+            ans.push_back(pow(i,2));
+        }
+        vector<vector<int>> dp(ans.size(), vector<int>(n+1,-1));
+        return solve(ans.size()-1, n, ans, dp);
+    }
+};
