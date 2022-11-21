b7bddf71c64fa1099dc820176c81fb0b3fd9dbd5
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-11-21 14:32:25 +0530

    Added solution - LeetHub

---
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+    sort(a.begin(), a.end());
+    
+    int i = 0;
+    int j = m-1;
+    long long mini_diff = INT_MAX;
+    while(j < n)
+    {
+        mini_diff = min(mini_diff, a[j] - a[i]);
+        j++;
+        i++;
+    }
+    
+    return mini_diff;
+    
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
