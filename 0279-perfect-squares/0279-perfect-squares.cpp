6079a2d4a22e96ebba26b6b9895ebce20c6b63db
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-11-22 11:12:02 +0530

    Time: 410 ms (45.42%), Space: 151.5 MB (8.91%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int solve(int index, int n, vector<int> &nums, vector<vector<int>> &dp)
+    int solve(int index, vector<int> &temp, int target, vector<vector<int>> &dp)
     {
+        if(target == 0) return 0;
         if(index == 0)
         {
-            return n;
+            if(target % temp[0] == 0) return target / temp[0];
+            else return 1e9;
         }
         
-        if(dp[index][n] != -1) return dp[index][n];
+        if(dp[index][target] != -1) return dp[index][target];
         
-        int not_pick = 0 + solve(index-1, n, nums, dp);
+        int not_pick = 0 + solve(index-1, temp, target, dp);
         int pick = 1e9;
-        if(nums[index] <= n)
+        if(temp[index] <= target)
         {
-            pick = 1 + solve(index, n - nums[index], nums, dp);
+            pick = 1 + solve(index, temp, target - temp[index], dp);
         }
         
-        return dp[index][n] = min(pick, not_pick);
+        return dp[index][target] = min(pick, not_pick);
     }
     int numSquares(int n) {
-        vector<int> ans;
-        for(int i = 1; i <= sqrt(n); i++)
+        vector<int> temp;
+        
+        for(int i = 1; i*i <= n; i++)
         {
-            ans.push_back(pow(i,2));
+            temp.push_back(i*i);
         }
-        vector<vector<int>> dp(ans.size(), vector<int>(n+1,-1));
-        return solve(ans.size()-1, n, ans, dp);
+        vector<vector<int>> dp(temp.size(), vector<int>(n+1,-1));
+        return solve(temp.size()-1, temp, n, dp);
     }
 };
