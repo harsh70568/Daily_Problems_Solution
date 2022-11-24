9e1a569964a84b42c174ca05bcbf04b080d33a38
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-11-25 02:06:07 +0530

    Time: 229 ms (80.98%), Space: 83.6 MB (52.61%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int j, int k, vector<int> &nums, vector<int> &multi, vector<vector<int>> &dp)
+    int solve(int i, int j, int k, vector<int> &nums, vector<int> &multipliers, vector<vector<int>> &dp)
     {
-        if(k == multi.size()) return 0;
+        if(k == multipliers.size()) return 0;
         
-        if(dp[k][i] != -1) return dp[k][i];
+        if(dp[i][k] != -1) return dp[i][k];
         
+        int pick_front = nums[i]*multipliers[k] + solve(i+1, j, k+1, nums, multipliers, dp);
+        int pick_back = nums[j]*multipliers[k] + solve(i, j-1, k+1, nums, multipliers, dp);
         
-        return dp[k][i] = max(nums[i]*multi[k] + solve(i+1, j, k+1, nums, multi, dp), nums[j]*multi[k] + solve(i, j-1, k+1, nums, multi, dp));
-        
-        
+        return dp[i][k] = max(pick_front, pick_back);
     }
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+    int maximumScore(vector<int>& nums, vector<int>& multi) {
         int n = nums.size();
-        int m = multipliers.size();
+        int m = multi.size();
         vector<vector<int>> dp(m, vector<int>(m+1,-1));
-        return solve(0, n-1, 0, nums, multipliers, dp);
+        return solve(0, n-1, 0, nums, multi, dp);
     }
 };
