dc1a509b766cd859dd517a798ca5953e96b6a041
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-11-24 21:38:10 +0530

    Added solution - LeetHub

--- Preorder traversal (Iterative) - GFG/preorder-traversal-iterative.cpp
+++ Preorder traversal (Iterative) - GFG/preorder-traversal-iterative.cpp
@@ -97,22 +97,21 @@ struct Node {
 */
 class Solution{
     public:
+    void solve(Node* root, vector<int> &ans)
+    {
+        if(root)
+        {
+            ans.push_back(root->data);
+            solve(root->left, ans);
+            solve(root->right, ans);
+        }
+    }
     vector<int> preOrder(Node* root)
     {
         //code here
         vector<int> ans;
-        stack<Node*> st;
-        st.push(root);
+        solve(root, ans);
         
-        while(!st.empty())
-        {
-            Node* curr = st.top();
-            st.pop();
-            ans.push_back(curr->data);
-            
-            if(curr->right) st.push(curr->right);
-            if(curr->left) st.push(curr->left);
-        }
         
         return ans;
     }
