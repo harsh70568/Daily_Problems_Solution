d8b41039ed69b9f34731e14eb21f4b6475fa6860
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-12-02 19:35:59 +0530

    Time: 206 ms (58.68%), Space: 20.8 MB (16.77%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        
         if(word1.length() != word2.length()) return false;
+        
         unordered_map<char, int> mpp;
         unordered_map<char, int> mpp2;
-        set<char> s1;
-        set<char> s2;
+        set<char> st;
         
         for(auto it : word1)
         {
             mpp[it]++;
-            s1.insert(it);
+            st.insert(it);
         }
         
         for(auto it : word2)
         {
             mpp2[it]++;
-            s2.insert(it);
+            if(st.find(it) == st.end()) return false;
         }
         
-        if(s1 != s2) return false;
-        vector<int> v1;
-        vector<int> v2;
         
+        vector<int> ans;
+        vector<int> temp;
         for(auto it : mpp)
         {
-            v1.push_back(it.second);
+            ans.push_back(it.second);
         }
         
         for(auto it : mpp2)
         {
-            v2.push_back(it.second);
+            temp.push_back(it.second);
         }
         
+        sort(ans.begin(), ans.end());
+        sort(temp.begin(), temp.end());
         
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-        
-        for(int i = 0; i < v1.size(); i++)
-        {
-            if(v1[i] != v2[i]) return false;
-        }
-        
-        return true;
+        return ans == temp;
     }
 };
