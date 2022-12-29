afe821c831d53daa73aacda1cbc3ee8238a60ee0
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2022-12-29 15:59:06 +0530

    Time: 98 ms (32.37%), Space: 20.6 MB (10.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid, vector<int> delRow, vector<int> delCol)
+    void dfs(int row, int col, vector<vector<char>> &grid, vector<vector<int>> &vis, vector<int> delRow, vector<int> delCol)
     {
         vis[row][col] = 1;
         
         int m = grid.size();
         int n = grid[0].size();
-        
         for(int i = 0; i < 4; i++)
         {
             int new_r = row + delRow[i];
             int new_c = col + delCol[i];
             
-            if(new_r < m && new_r >= 0 && new_c < n && new_c >= 0)
+            if(new_r >= 0 && new_r < m && new_c >= 0 && new_c < n)
             {
                 if(vis[new_r][new_c] == 0 && grid[new_r][new_c] == '1')
                 {
-                    dfs(new_r, new_c, vis, grid, delRow, delCol);
+                    dfs(new_r, new_c, grid, vis, delRow, delCol);
                 }
             }
         }
@@ -24,19 +23,19 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n,0));
         
         int cnt = 0;
-        vector<vector<int>> vis(m, vector<int>(n,0));
-        vector<int> delRow = {-1, 0, +1, 0};
+        vector<int> delRow = {+1, 0, -1, 0};
         vector<int> delCol = {0, +1, 0, -1};
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                if(vis[i][j] == 0 && grid[i][j] == '1')
+                if(grid[i][j] == '1' && vis[i][j] == 0)
                 {
                     cnt++;
-                    dfs(i, j, vis, grid, delRow, delCol);
+                    dfs(i, j, grid, vis, delRow, delCol);
                 }
             }
         }
