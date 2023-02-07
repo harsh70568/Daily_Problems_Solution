af380c04c9c27c65ec90dbffc7d29ca7c18e59ad
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-02-08 01:34:12 +0530

    Time: 75 ms (44.21%), Space: 26.1 MB (36.11%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    bool is_possible(vector<int> weights, int days, int mid)
+    bool is_possible(vector<int> &weights, int mid, int days)
     {
-        int count = 1;
         int sum = 0;
-        
+        int cnt = 1;
         for(int i = 0; i < weights.size(); i++)
         {
             if(sum + weights[i] <= mid)
@@ -13,39 +12,34 @@ public:
             }
             else
             {
-                count++;
-                if(count > days || weights[i] > mid)
-                {
-                    return false;
-                }
+                cnt++;
+                if(weights[i] > mid || cnt > days) return false;
                 sum = weights[i];
             }
         }
+        
         return true;
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int start = *min_element(weights.begin(),weights.end());
-        int sum = 0;
-        for(int i = 0; i < weights.size(); i++)
-        {
-            sum += weights[i];
-        }
-        int end = sum;
-        int ans = -1;
+    int shipWithinDays(vector<int>& w, int days) {
+        int sum = accumulate(w.begin(), w.end(), 0);
         
-        while(start <= end)
+        int low = 1;
+        int high = sum;
+        int ans = 0;
+        while(low <= high)
         {
-            int mid = (start+end)/2;
-            if(is_possible(weights,days,mid))
+            int mid = (low + high) >> 1;
+            if(is_possible(w, mid, days))
             {
                 ans = mid;
-                end = mid-1;
+                high = mid - 1;
             }
             else
             {
-                start = mid+1;
+                low = mid + 1;
             }
         }
+        
         return ans;
     }
 };
