a76425f6ae56b797f3b2af57d7f7c534d08196de
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-02-11 00:12:52 +0530

    Time: 215 ms (47.26%), Space: 59 MB (32.37%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int> &vis, vector<int> &pathVis, vector<vector<int>> &g)
-    {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        
-        for(auto it : g[node])
-        {
-            if(vis[it] == 0)
-            {
-                if(dfs(it, vis, pathVis, g) == true) return true;
-            }
-            else if(pathVis[it] == 1) return true;
-        }
-        pathVis[node] = 0;
-        
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& g) {
         int n = g.size();
         vector<int> ans;
-        vector<int> vis(n,0);
-        vector<int> pathVis(n,0);
         
+        vector<int> adj[n];
+        vector<int> indegree(n);
+        // reverse edges
         for(int i = 0; i < n; i++)
         {
-            if(vis[i] == 0)
+            // edge stored = i -> it
+            // now stored as = it -> i
+            for(auto it : g[i])
             {
-                dfs(i, vis, pathVis, g);
+                adj[it].push_back(i);
+                indegree[i]++;
             }
         }
         
-        
+        queue<int> q;
         for(int i = 0; i < n; i++)
         {
-            if(pathVis[i] == 0) ans.push_back(i);
+            if(indegree[i] == 0)
+            {
+                q.push(i);
+            }
         }
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            ans.push_back(node);
+            
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        
+        sort(ans.begin(), ans.end());
         
         return ans;
     }
