9f27532cfda75b01c9b7508c7dbe80040d073cb0
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-02-10 23:28:39 +0530

    Time: 145 ms (97.09%), Space: 47.6 MB (79.25%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int> &vis, vector<int> &pathVis, vector<vector<int>> &graph)
+    bool dfs(int node, vector<int> &vis, vector<int> &pathVis, vector<vector<int>> &g)
     {
         vis[node] = 1;
         pathVis[node] = 1;
         
-        for(auto it : graph[node])
+        for(auto it : g[node])
         {
             if(vis[it] == 0)
             {
-                if(dfs(it, vis, pathVis, graph) == true) return true;
+                if(dfs(it, vis, pathVis, g) == true) return true;
             }
             else if(pathVis[it] == 1) return true;
         }
-        
         pathVis[node] = 0;
+        
         return false;
     }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
+    vector<int> eventualSafeNodes(vector<vector<int>>& g) {
+        int n = g.size();
+        vector<int> ans;
         vector<int> vis(n,0);
         vector<int> pathVis(n,0);
         
-        vector<int> ans;
         for(int i = 0; i < n; i++)
         {
             if(vis[i] == 0)
             {
-                dfs(i, vis, pathVis, graph);
+                dfs(i, vis, pathVis, g);
             }
         }
         
-        // the nodes which dp not forms cycle or do not leads to the cycle will end up to the safe state.
-        for(int i = 0; i < pathVis.size(); i++)
+        
+        for(int i = 0; i < n; i++)
         {
             if(pathVis[i] == 0) ans.push_back(i);
         }
