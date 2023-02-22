1bc9516c45553aed5fe36d9cd0187627cdd97f78
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-02-22 10:23:52 +0530

    Time: 88 ms (19.10%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -6,30 +6,33 @@ public:
         int cnt = 1;
         for(int i = 0; i < weights.size(); i++)
         {
-            if(sum + weights[i] <= mid)
+            if(sum + weights[i] > mid)
             {
-                sum += weights[i];
+                cnt++;
+                sum = weights[i];
+                if(sum > mid) return false;
             }
             else
             {
-                cnt++;
-                if(weights[i] > mid || cnt > days) return false;
-                sum = weights[i];
+                sum += weights[i];
+                //if(sum > mid) return false;
             }
         }
         
-        return true;
+        if(sum > mid) return false;
+        
+        return cnt <= days;
     }
-    int shipWithinDays(vector<int>& w, int days) {
-        int sum = accumulate(w.begin(), w.end(), 0);
+    int shipWithinDays(vector<int>& weights, int days) {
+        int sum = accumulate(weights.begin(), weights.end(), 0);
         
         int low = 1;
         int high = sum;
-        int ans = 0;
+        int ans = -1;
         while(low <= high)
         {
             int mid = (low + high) >> 1;
-            if(is_possible(w, mid, days))
+            if(is_possible(weights, mid, days))
             {
                 ans = mid;
                 high = mid - 1;
