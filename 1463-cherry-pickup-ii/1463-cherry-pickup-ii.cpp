c19b23b558c2bf8a3ef80c98026f2c051baa2b0a
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-14 22:54:29 +0530

    Time: 125 ms (77.60%), Space: 17.3 MB (10.67%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int solve(int i, int j1, int j2, int m, int n, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
+    int solve(int i, int j1, int j2, vector<vector<int>> &grid, int m, int n, vector<vector<vector<int>>> &dp)
     {
-        if(j1 < 0 || j1 > n-1 || j2 < 0 || j2 > n-1) return -1e9;
+        if(i < 0 || i >= m || j1 < 0 || j1 >= n || j2 < 0 || j2 >= n) return -1e9;
         if(i == m-1)
         {
             if(j1 == j2) return grid[i][j1];
@@ -16,19 +16,18 @@ public:
         {
             for(int y = -1; y <= 1; y++)
             {
-                if(j1 == j2) maxi = max(maxi,grid[i][j1] + solve(i+1,j1+x,j2+y,m,n,grid,dp));
-                else
-                {
-                    maxi = max(maxi,grid[i][j1] + grid[i][j2] + solve(i+1,j1+x,j2+y,m,n,grid,dp));
-                }
+                if(j1 == j2) maxi = max(maxi, grid[i][j1] + solve(i+1, j1+x, j2+y, grid, m, n, dp));
+                else maxi = max(maxi, grid[i][j1] + grid[i][j2] + solve(i+1, j1+x, j2+y, grid, m, n, dp));
             }
         }
+        
         return dp[i][j1][j2] = maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int> (n,-1)));
-        return solve(0,0,n-1,m,n,grid,dp);
+        
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n+1, vector<int>(n+1,-1)));
+        return solve(0, 0, n-1, grid, m, n, dp);
     }
 };
