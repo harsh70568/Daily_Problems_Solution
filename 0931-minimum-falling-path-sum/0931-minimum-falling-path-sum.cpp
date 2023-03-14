a218c1d559691c2f66d736ac86055df1a7b4f6b8
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-14 22:30:09 +0530

    Time: 24 ms (10.80%), Space: 10.4 MB (7.75%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>> &grid, int m, int n, vector<vector<int>> &dp)
+    int solve(int i, int j, vector<vector<int>> &matrix, int m, int n, vector<vector<int>> &dp)
     {
-        // base case
-        if(j < 0 || j > n-1) return 1e9;
-        if(i == m-1) return grid[i][j];
+        if(i < 0 || i >= m || j < 0 || j >= n) return 1e9;
+        if(i == m-1) return matrix[i][j];
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        // recursion calls
-        int left = grid[i][j] +  solve(i+1, j-1, grid, m, n, dp);
-        int right = grid[i][j] +  solve(i+1, j+1, grid, m, n, dp);
-        int down = grid[i][j] + solve(i+1, j, grid, m, n, dp);
+        int left_diagonal = matrix[i][j] + solve(i+1, j-1, matrix, m, n, dp);
+        int down = matrix[i][j] + solve(i+1, j, matrix, m, n, dp);
+        int right_diagonal = matrix[i][j] + solve(i+1, j+1, matrix, m, n, dp);
         
-        // minimum out of all calls
-        return dp[i][j] = min({left, right, down});
+        return dp[i][j] = min({left_diagonal, down, right_diagonal});
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n+1,-1));
         
-        int ans = INT_MAX;
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        int mini = INT_MAX;
         for(int i = 0; i < n; i++)
         {
-            ans = min(ans, solve(0, i, matrix, m, n, dp));
+            mini = min(mini, solve(0, i, matrix, m, n, dp));
         }
         
-        
-        return ans;
+        return mini;
     }
 };
