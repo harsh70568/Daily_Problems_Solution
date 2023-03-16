53f83db35754499f579d223923b9ecfe09e1c073
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-16 15:08:09 +0530

    Time: 314 ms (74.41%), Space: 81.5 MB (63.26%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int solve(int index, vector<vector<int>> &events, vector<int> &v, int k, vector<vector<int>> &dp)
+    int solve(int index, vector<vector<int>> &events, vector<int> &start, int k, vector<vector<int>> &dp)
     {
         if(k == 0) return 0;
-        if(index == events.size()) return 0;
+        if(index >= events.size()) return 0;
         
         if(dp[index][k] != -1) return dp[index][k];
         
-        int next_idx = upper_bound(v.begin(), v.end(), events[index][1]) - v.begin();
-        int not_pick = 0 + solve(index+1, events, v, k, dp);
-        int pick = events[index][2] + solve(next_idx, events, v, k-1, dp);
+        int next_idx = upper_bound(start.begin(), start.end(), events[index][1]) - start.begin();
+        int not_pick = solve(index+1, events, start, k, dp);
+        int pick = events[index][2] + solve(next_idx, events, start, k-1, dp);
         
         return dp[index][k] = max(pick, not_pick);
     }
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
         sort(events.begin(), events.end());
-        
-        vector<int> v;
+        vector<int> start;
         for(auto it : events)
         {
-            v.push_back(it[0]);
+            start.push_back(it[0]);
         }
-        vector<vector<int>> dp(n, vector<int>(k+1,-1));
-        return solve(0, events, v, k, dp);
+        
+        vector<vector<int>> dp(n+1, vector<int>(k+1,-1));
+        return solve(0, events, start, k, dp);
     }
 };
