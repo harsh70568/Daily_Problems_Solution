ef2c29939a49d658eaa309596190fa7e529e7fce
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-16 13:59:33 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -1,54 +1,50 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for C++
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
+// } Driver Code Ends
 // User function Template for C++
 
 class Solution{
   public:
+  int solve(int index, int len[], int price[], int n, vector<vector<int>> &dp)
+  {
+      if(index == 0)
+      {
+          if(len[0] <= n) return n*price[0];
+          else return 0;
+      }
+      
+      if(dp[index][n] != -1) return dp[index][n];
+      
+      int not_pick = solve(index-1, len, price, n, dp);
+      int pick = 0;
+      if(len[index] <= n)
+      {
+          pick = max(price[index] + solve(index, len, price, n - len[index], dp), solve(index-1, len, price, n, dp));
+      }
+      
+      return dp[index][n] = max(pick, not_pick);
+  }
     int cutRod(int price[], int n) {
-        
         //code here
         int len[n];
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i <= n; i++)
         {
             len[i] = i+1;
         }
-        int dp[n+1][n+1];
-        for(int i = 0; i < n+1; i++)
-        {
-            for(int j = 0; j < n+1; j++)
-            {
-                if(i == 0 || j == 0)
-                {
-                    dp[i][j] = 0;
-                }
-            }
-        }
+        //for(int i = 0; i < n; i++) cout<<len[i]<<" ";
+        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        return solve(n-1, len, price, n, dp);
         
-        for(int i = 1; i < n+1; i++)
-        {
-            for(int j = 1; j < n+1; j++)
-            {
-                if(len[i-1] <= j)
-                {
-                    dp[i][j] = max(price[i-1]+dp[i][j-len[i-1]],dp[i-1][j]);
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][n];
+        
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main() {
     int t;
@@ -65,4 +61,5 @@ int main() {
         cout << ob.cutRod(a, n) << endl;
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
