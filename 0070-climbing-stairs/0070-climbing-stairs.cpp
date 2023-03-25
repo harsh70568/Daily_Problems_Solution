776939de97560aaba97e0e715aede9797f5787c1
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-25 22:02:21 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (21.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int solve(int n, vector<int> &dp)
     {
-        if(n == 1 || n == 2) return n;
+        if(n == 1 || n == 0) return 1;
         
         if(dp[n] != -1) return dp[n];
+        
         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
     int climbStairs(int n) {
