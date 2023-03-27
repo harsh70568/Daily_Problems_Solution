9861b8ee7a556fd9edc5883305810e92cc838f80
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-03-28 00:36:19 +0530

    Time: 3 ms (49.78%), Space: 8.3 MB (97.53%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,18 +11,30 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &ans)
-     {
-        if(root)
-        {
-            solve(root->left, ans);   // left
-            solve(root->right, ans);  // right
-            ans.push_back(root->val); // root
-        }
-     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(root, ans);
+        stack<int> st;
+        stack<TreeNode*> st2;
+        if(root == NULL) return ans;
+        st2.push(root);
+        
+        
+        while(!st2.empty())
+        {
+            auto node = st2.top();
+            st2.pop();
+            
+            st.push(node->val);
+            if(node->left) st2.push(node->left);
+            if(node->right) st2.push(node->right);
+        }
+        
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
         return ans;
     }
 };
