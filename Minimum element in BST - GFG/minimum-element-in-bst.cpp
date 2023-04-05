33bc8fd4e2dff7ec3c527642ba51eb83e57656e1
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-06 00:03:00 +0530

    Added solution - LeetHub

--- Minimum element in BST - GFG/minimum-element-in-bst.cpp
+++ Minimum element in BST - GFG/minimum-element-in-bst.cpp
@@ -94,6 +94,23 @@ int main() {
 
 
 // Function to find the minimum element in the given BST.
+
+/*
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
 int minValue(Node* root) {
     // Code here
     if(root == NULL) return -1;
