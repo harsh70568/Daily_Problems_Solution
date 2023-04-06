c3f14ca2b86d7210888241b67adcbb00ac362416
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-06 14:24:10 +0530

    Time: 62 ms (14.09%), Space: 20 MB (17.58%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,28 +2,41 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.length();
-        int ones = 0;
-        int zero = 0;
         
-        for(int i = 0; i < n; i++)
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        
+        if(s[0] == '1') prefix[0] = 1;
+        else prefix[0] = 0;
+        
+        if(s[n-1] == '0') suffix[n-1] = 1;
+        else suffix[n-1] = 0;
+        
+        for(int i = 1; i < n; i++)
         {
-            if(s[i] == '0')
-            {
-                if(ones >= 1)
-                {
-                    zero++;
-                }
-            }
-            else
-            {
-                ones++;
-            }
-            if(ones < zero)
-            {
-                zero = ones;
-            }
+            if(s[i] == '1') prefix[i] = prefix[i-1] + 1;
+            else prefix[i] = prefix[i-1];
         }
         
-        return zero;
+        for(int i = n-2; i >= 0; i--)
+        {
+            if(s[i] == '0') suffix[i] = suffix[i+1] + 1;
+            else suffix[i] = suffix[i+1];
+        }
+        
+        //for(auto it : prefix) cout<<it<<" ";
+        //for(auto it : suffix) cout<<it<<" ";
+        
+        int mini = 1e9;
+        for(int i = 1; i < n-1; i++)
+        {
+            int val = prefix[i-1] + suffix[i+1];
+            mini = min(mini, val);
+        }
+        
+        mini = min(mini, suffix[1]);
+        mini = min(mini, prefix[n-2]);
+        
+        return mini;
     }
 };
