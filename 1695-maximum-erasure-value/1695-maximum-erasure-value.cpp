d2665b0d33d349824a71b62beb3ff40a859deb2a
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-17 00:10:45 +0530

    Time: 558 ms (14.39%), Space: 133.4 MB (10.06%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,22 +4,23 @@ public:
         int i = 0;
         int j = 0;
         
-        int max_sum = INT_MIN;
+        map<int, int> mpp;
         int sum = 0;
-        unordered_map<int, int> mpp;
+        int max_sum = 0;
+        
         while(j < nums.size())
         {
-            sum += nums[j];
             mpp[nums[j]]++;
+            sum += nums[j];
             
-            if(mpp.size() == j-i+1)
+            if(mpp.size() == j-i+1) // all unique characters
             {
-                 max_sum = max(max_sum, sum);
+                max_sum = max(max_sum, sum);
                 j++;
             }
             else
             {
-                while(mpp.size() != j-i+1)
+                while(mpp.size() < j-i+1)
                 {
                     sum -= nums[i];
                     mpp[nums[i]]--;
@@ -28,7 +29,6 @@ public:
                 }
                 j++;
             }
-           
         }
         
         return max_sum;
