9a54ae1920798b13e0417d218e796dc5ddc95c2d
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-16 21:48:29 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,38 +29,36 @@ int main() {
 // } Driver Code Ends
 
 
-vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
-    int i = 0;
-    int j = 0;
-    queue<long long> q;
-    vector<long long> ans;
-    while(j < N)
-    {
-        if(A[j] < 0) q.push(A[j]);
-        
-        if(j-i+1 < K)
-        {
-            j++;
-        }
-        else if(j-i+1 == K)
-        {
-            if(q.size() == 0)
+vector<long long> printFirstNegativeInteger(long long int arr[],long long int n, long long int k) {
+            vector<long long> ans;
+            queue<long long> q;
+            
+            int i = 0;
+            int j = 0;
+            while(j < n)
             {
-                ans.push_back(0);
-                i++;
-                j++;
+                if(arr[j] < 0) q.push(arr[j]);
+                
+                if(j-i+1 < k)
+                {
+                    j++;
+                }
+                else
+                {
+                    if(q.empty()) ans.push_back(0);
+                    else
+                    {
+                        ans.push_back(q.front());
+                        
+                        if(arr[i] == q.front())
+                        q.pop();
+                        
+                    }
+                    i++;
+                    j++;
+                }
             }
-            else
-            {
-                ans.push_back(q.front());
-                if(q.front() == A[i]) q.pop();
-                i++;
-                j++;
-            }
-        }
-    }
-    
-    return ans;
-                                                 
-                                                 
-}
+            
+            return ans;
+                    
+ }
