34b97786a75318b37fa764fe8b96b5052f23f8cd
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-17 15:30:39 +0530

    Time: 19 ms (45.87%), Space: 8.8 MB (44.06%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> freq1(26,0);
-        vector<int> freq2(26,0);
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
         
-        for(int i = 0; i < p.length(); i++)
-        {
-            freq2[p[i] - 'a']++;
-        }
+        for(int i = 0; i < p.length(); i++) freq2[p[i] - 'a']++;
         
         int i = 0;
         int j = 0;
-        vector<int> ans;
         int k = p.length();
+        vector<int> ans;
         while(j < s.length())
         {
-            if(p.find(s[j]) != std::string::npos)
+            if(p.find(s[j]) != string::npos)
             {
-                freq1[s[j] - 'a']++;
+                freq1[s[j] - 'a']++; // contribution added
             }
             
-            if(j-i+1 < k) j++;
-            else if(j-i+1 == k)
+            if(j-i+1 < k)
+            {
+                j++;
+            }
+            else if(j-i+1 == k)   // window of size k
             {
                 if(freq1 == freq2) ans.push_back(i);
-                if(p.find(s[i]) != std::string::npos) freq1[s[i] - 'a']--;
+                if(p.find(s[i]) != string::npos) freq1[s[i] - 'a']--;   // contribution withdrawal
+                
+                // slide window
                 i++;
                 j++;
             }
-            
         }
         
         return ans;
     }
-    
 };
