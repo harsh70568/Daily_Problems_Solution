2df49ea90a0085f7abf16c37ba0b1ebd7589acb5
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-04-17 15:55:10 +0530

    Time: 178 ms (37.39%), Space: 107.3 MB (98.46%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans = 0;
-        long long curr_count = 0;
+        
+        long long cnt = 0;
         for(int i = 0; i < nums.size(); i++)
         {
             if(nums[i] == 0)
             {
-                curr_count++;
+                cnt++;
             }
             else
             {
-                ans += curr_count*(curr_count+1)/2;
-                curr_count = 0;
+                ans += ((cnt*(cnt+1)) / 2);
+                cnt = 0;
             }
         }
-        ans += curr_count*(curr_count+1)/2;
+        
+        ans += ((cnt*(cnt+1)) / 2);
         return ans;
     }
 };
