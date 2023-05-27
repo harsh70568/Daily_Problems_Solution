2edfdba5ff33b48efb8891cff84d5f6c49ba9e41
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-05-28 00:50:47 +0530

    Time: 477 ms (21.20%), Space: 18 MB (11.95%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -6,20 +6,19 @@ public:
         
         if(dp[index] != -1) return dp[index];
         
-        int max_jumps = index + nums[index];
-        int ans = 1e9;
-        for(int i = index; i < min(n, max_jumps); i++)
+        int mini = 1e9;
+        int jumps = nums[index] + index;
+        for(int i = index; i < min(n, jumps); i++)
         {
             int temp = 1 + solve(i+1, nums, n, dp);
-            ans = min(ans, temp);
+            mini = min(mini, temp);
         }
         
-        return dp[index] = ans;
-        
+        return dp[index] = mini;
     }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,-1);
         return solve(0, nums, n, dp);
     }
 };
