ab4c7624b06ad08ffacf65ddd14e2ec9aacbe4d7
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-05-29 21:20:51 +0530

    Time: 66 ms (43.21%), Space: 32.9 MB (88.19%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -0,0 +1,14 @@
+class ParkingSystem
+{
+public:
+    int space[3];
+    ParkingSystem(int big, int medium, int small)
+    {
+        space[0] = big, space[1] = medium, space[2] = small;
+    }
+    bool addCar(int carType)
+    {
+        space[carType - 1]--;
+        return (bool)(space[carType-1] >= 0);
+    };
+};
