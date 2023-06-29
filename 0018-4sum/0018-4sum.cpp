cf6a0793c8904778c968cb482e54dc7744b8852d
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-06-29 11:45:29 +0530

    Time: 1283 ms (8.83%), Space: 244.4 MB (6.68%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
         vector<vector<int>> ans;
-        
+        if(nums.size() < 4) return ans;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < n-3; i++)
+        for(int i = 0; i < nums.size()-3; i++)
         {
-            for(int j = i+1; j < n-2; j++)
+            for(int j = i+1; j < nums.size()-2; j++)
             {
                 long long req_sum = 1LL*target - nums[i] - nums[j];
-                int left = j+1;
-                int right = n-1;
+                int low = j+1;
+                int high = nums.size()-1;
                 
-                while(left < right)
+                while(low < high)
                 {
                     vector<int> temp(4);
-                    if(nums[left] + nums[right] == req_sum)
+                    if(nums[low] + nums[high] == req_sum)
                     {
                         temp[0] = nums[i];
                         temp[1] = nums[j];
-                        temp[2] = nums[left];
-                        temp[3] = nums[right];
-                        
+                        temp[2] = nums[low];
+                        temp[3] = nums[high];
                         ans.push_back(temp);
-                        while(left < right && temp[2] == nums[left]) left++;
-                        while(left && right && temp[3] == nums[right]) right--;
+                        
+                        while(low < high && nums[low] == temp[2]) low++;
+                        while(low < high && nums[high] == temp[3]) high--;
                     }
-                    else if(nums[left] + nums[right] < req_sum)
+                    else if(nums[low] + nums[high] < req_sum)
                     {
-                        left++;
+                        low++;
                     }
                     else
                     {
-                        right--;
+                        high--;
                     }
                     
                 }
-                while(i < n-1 && nums[i] == nums[i+1]) i++;
-                while(j < n-1 && nums[j] == nums[j+1]) j++;
+                
+                while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+                while(j+1 < nums.size() && nums[j] == nums[j+1]) j++;
             }
-            
         }
         
         return ans;
