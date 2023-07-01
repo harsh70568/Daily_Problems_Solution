b778f136025ac64a8b3c6b59d44fc3ca21584704
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-01 22:09:51 +0530

    Time: 119 ms (55.54%), Space: 49.7 MB (11.20%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int solve(int index, vector<int> &coins, int amount, vector<vector<int>> &dp)
     {
+        if(amount == 0) return 0;
         if(index == 0)
         {
-            if(amount % coins[0] == 0) return amount / coins[0];
+            if((amount % coins[0]) == 0) return (amount / coins[0]);
             else return 1e9;
         }
         
         if(dp[index][amount] != -1) return dp[index][amount];
         
+        int not_pick = solve(index-1, coins, amount, dp);
         int pick = 1e9;
         if(coins[index] <= amount)
         {
-            pick = 1 + solve(index, coins, amount  - coins[index], dp);
+            pick = 1 + solve(index, coins, amount - coins[index], dp);
         }
-        int not_pick = 0 + solve(index-1, coins, amount, dp);
         
         return dp[index][amount] = min(pick, not_pick);
     }
@@ -23,7 +24,7 @@ public:
         int n = coins.size();
         vector<vector<int>> dp(n+1, vector<int>(amount+1,-1));
         int ans = solve(n-1, coins, amount, dp);
-        
+
         return ans == 1e9 ? -1 : ans;
     }
 };
