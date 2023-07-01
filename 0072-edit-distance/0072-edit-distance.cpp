b4dd44ae75e79f6e96f08842a79616cd270aa0b5
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-01 23:04:57 +0530

    Time: 18 ms (49.90%), Space: 9.3 MB (21.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,24 +7,20 @@ public:
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(word1[i] == word2[j])
-        {
-            return dp[i][j] = solve(i-1, j-1, word1, word2, dp);
-        }
+        
+        if(word1[i] == word2[j]) return solve(i-1, j-1, word1, word2, dp);
         else
         {
             int insert = 1 + solve(i, j-1, word1, word2, dp);
             int del = 1 + solve(i-1, j, word1, word2, dp);
-            int replace = 1 + solve(i-1, j-1, word1, word2, dp);
+            int rep = 1 + solve(i-1, j-1, word1, word2, dp);
             
-            return dp[i][j] = min({insert, del, replace});
+            return dp[i][j] = min({insert, del, rep});
         }
     }
     int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return solve(n-1, m-1, word1, word2, dp);
+        int m = word1.length(), n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        return solve(m-1, n-1, word1, word2, dp);
     }
 };
