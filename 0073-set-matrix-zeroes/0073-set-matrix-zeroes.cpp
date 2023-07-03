70169bed3ec3903040a4e0f7c0dc32c114bdeca7
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-04 02:30:24 +0530

    Time: 14 ms (80.05%), Space: 13.2 MB (52.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,25 +3,34 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        stack<pair<int, int>> st;
+        
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                if(matrix[i][j] == 0) st.push({i, j});
+                if(matrix[i][j] == 0)
+                {
+                    for(int k = 0; k < n; k++) if(matrix[i][k] != 0) matrix[i][k] = 1e9+7;
+                    for(int k = 0; k < m; k++) if(matrix[k][j] != 0) matrix[k][j] = 1e9+7;
+                }
             }
         }
         
-        while(!st.empty())
-        {
-            int r = st.top().first;
-            int c = st.top().second;
-            st.pop();
-            
-            for(int j = 0; j < n; j++) matrix[r][j] = 0;
-            for(int i = 0; i < m; i++) matrix[i][c] = 0;
-        }
+        // for(int i = 0; i < m; i++)
+        // {
+        //     for(int j = 0; j < n; j++)
+        //     {
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         
-        //return matrix;
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(matrix[i][j] == 1e9+7) matrix[i][j] = 0;
+            }
+        }
     }
 };
