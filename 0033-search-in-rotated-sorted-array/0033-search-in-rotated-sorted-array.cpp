1f9b5b931755e419f3ad59689c47e8f421a33cee
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-03 14:06:31 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (50.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -11,28 +11,15 @@ public:
             if(nums[mid] == target) return mid;
             else if(nums[low] <= nums[mid])
             {
-                if(nums[low] <= target && target <= nums[mid])
-                {
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
+                if(nums[low] <= target && nums[mid] >= target) high = mid - 1;
+                else low = mid + 1;
             }
             else
             {
-                if(nums[mid] <= target && target <= nums[high])
-                {
-                    low = mid + 1;
-                }
-                else
-                {
-                    high = mid - 1;
-                }
+                if(nums[mid] <= target && nums[high] >= target) low = mid + 1;
+                else high = mid - 1;
             }
         }
-        
         return -1;
     }
 };
