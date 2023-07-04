f3bc8c59de5ade3f9b87fc3445757c4313af6c33
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-04 15:12:15 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.cpp
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -12,19 +12,16 @@ public:
     vector<int> minPartition(int N)
     {
         // code here
-        vector<int> temp = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
-        
         vector<int> ans;
-        
+        vector<int> temp = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
         for(int i = temp.size()-1; i >= 0; i--)
         {
             while(temp[i] <= N)
             {
-                ans.push_back(temp[i]);
-                N -= temp[i];
+                 N -= temp[i];
+                 ans.push_back(temp[i]);
             }
         }
-        
         return ans;
     }
 };
