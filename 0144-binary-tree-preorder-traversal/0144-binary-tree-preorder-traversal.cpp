d354da9c4d07dae5eeb5338486d3a0ae18ba6af0
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-04 12:09:57 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (13.89%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,18 +11,23 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &ans)
-    {
-        if(root)
-        {
-            ans.push_back(root->val);
-            solve(root->left, ans);
-            solve(root->right, ans);
-        }
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(root, ans);
+        if(root == NULL) return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while(!st.empty())
+        {
+            TreeNode* curr = st.top();
+            st.pop();
+            
+            ans.push_back(curr->val);
+            
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+        }
+        
         return ans;
     }
 };
