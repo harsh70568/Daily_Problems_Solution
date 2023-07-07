95d35bc8351032190e743be691db57f76f99892f
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2023-07-08 02:01:02 +0530

    Added solution - LeetHub

---
+++ Power Set - GFG/power-set.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	void solve(int index, string s, string temp, vector<string> &ans)
+	{
+	    if(index >= s.length())
+	    {
+	        if(temp != "") ans.push_back(temp);
+	        return;
+	    }
+	    
+	    solve(index+1, s, temp, ans);
+	    
+	    temp += s[index];
+	    solve(index+1, s, temp, ans);
+	    temp.pop_back();
+	}
+		vector<string> AllPossibleStrings(string s){
+		    vector<string> ans;
+		    string temp = "";
+		    solve(0, s, temp, ans);
+		    sort(ans.begin(), ans.end());
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
